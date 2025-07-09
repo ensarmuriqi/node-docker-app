75115c51d178ee242e0a9fb733419bd7b19f2526
ensarmuriqi <95577829+ensarmuriqi@users.noreply.github.com>
2025-07-09 11:15:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker and Jenkins!');
+    res.send('Helo, Docker and Jenkins!');
 });
 
 const port = process.env.PORT || 3000;
